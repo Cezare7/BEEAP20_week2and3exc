3d867d84f239d97b95f9e810907783c44d334249
Benjamin Democh <benjamin.democh@student.hamk.fi>
2021-04-18 19:59:39 +0300

    fixed warnings

--- main.py
+++ main.py
@@ -5,21 +5,24 @@ from tkinter import ttk
 from tkinter import filedialog as fd
 import tkinter.messagebox as tkBox
 import tkinter.font as tkFont
-
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
+
+# %% Class App
 class App:
+
+
+# %% Initialize
     def __init__(self, root):
-        # setting title
-        root.title("Energy Usage 2010") # change this to proper name when you think of one.
-        # setting window size
+
+        root.title("Energy Usage 2010")
         width = 600
         height = 500
         screenwidth = root.winfo_screenwidth()
         screenheight = root.winfo_screenheight()
-        alignstr = '%dx%d+%d+%d' % (width, height,(screenwidth - width) / 2, (screenheight - height) / 2)
+        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2,
+                                    (screenheight - height) / 2)
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
 
@@ -29,7 +32,7 @@ class App:
         self.__Button_01["font"] = ft
         self.__Button_01["fg"] = "#000000"
         self.__Button_01["justify"] = "center"
-        self.__Button_01["text"] = "Open CSV file"  # this is the button name change it to a propper if needed
+        self.__Button_01["text"] = "Open CSV file"
         self.__Button_01.place(x=70, y=50, width=90, height=25)
         self.__Button_01["command"] = self.Button_01_command
         self.__ComboBox_01 = ttk.Combobox(root)
@@ -58,35 +61,34 @@ class App:
         self.__GLineEdit_700 = tk.Canvas(root)
         self.__GLineEdit_700.place(x=310, y=295, width=250, height=155)
 
+# %%  Button command
+
     def Button_01_command(self):
+
         filePath = fd.askopenfilename(initialdir='.')
         try:
             self.__df = pd.read_csv(filePath)
             self.__df = self.__df.dropna()
-            self.__ComboBox_01['values'] = list(self.__df['COMMUNITY AREA NAME'].unique())
-        except:
-            # quick and dirty, desired behavior would be to show a notification pop up that says
-            # "nope!"
-            tkBox.showinfo('This is an ugly Error!', 'Not that one, choose another')
-            # print('nope')
+            self.__ComboBox_01['values'] = list(self.__df['COMMUNITY AREA'
+                                                          ' NAME'].unique())
+        except filePath.DoesNotExist:
+            tkBox.showinfo('This is an ugly Error!',
+                           'Not that one, choose another')
 
-    # desired behavior: select one area, show 4 plots drawn on 4 canvases of that area: 
-    # top left: bar chart, average KWH by month
-    # top right: bar chart, average THERM by month
-    # bottom left and bottom right up to you
+# %% Combobox
+
     def __comboBoxCb(self, event=None):
-        self.__subdf = self.__df.loc[self.__df['COMMUNITY AREA NAME'] == self.__ComboBox_01.get()]
-
+        self.__subdf = self.__df.loc[self.__df['COMMUNITY AREA NAME'
+                                               ''] == self.__ComboBox_01.get()]
 
         figure1 = plt.figure(dpi=50)
         ax1 = figure1.add_subplot(111)
         bar1 = FigureCanvasTkAgg(figure1, root)
         bar1.get_tk_widget().place(x=50, y=130, width=250, height=155)
         kwhJanIdx = (self.__subdf.columns.get_loc('KWH JANUARY 2010'))
-        print(kwhJanIdx)
-        df1 = self.__subdf.iloc[:, range(kwhJanIdx, kwhJanIdx+12)].mean().plot.bar(ax=ax1)
+        self.__subdf.iloc[:, range(kwhJanIdx,
+                                   kwhJanIdx+12)].mean().plot.bar(ax=ax1)
         ax1.set_title('KWH mean')
-
 
         figure1 = plt.figure(dpi=50)
         ax1 = figure1.add_subplot(111)
@@ -94,29 +96,32 @@ class App:
         bar1.get_tk_widget().place(x=310, y=130, width=250, height=155)
         kwhJanIdx = (self.__subdf.columns.get_loc('THERM JANUARY 2010'))
         print(kwhJanIdx)
-        df1 = self.__subdf.iloc[:, range(kwhJanIdx, kwhJanIdx+12)].mean().plot.bar(ax=ax1)
+        df1 = self.__subdf.iloc[:, range(kwhJanIdx,
+                                         kwhJanIdx+12)].mean().plot.bar(ax=ax1)
         ax1.set_title('THERM mean')
-        
-        
+
         figure1 = plt.figure(dpi=50)
         ax1 = figure1.add_subplot(111)
         bar1 = FigureCanvasTkAgg(figure1, root)
         bar1.get_tk_widget().place(x=50, y=295, width=250, height=155)
         kwhJanIdx = (self.__subdf.columns.get_loc('KWH JANUARY 2010'))
         print(kwhJanIdx)
-        df1 = self.__subdf.iloc[:, range(kwhJanIdx, kwhJanIdx+12)].max().plot.bar(ax=ax1)
+        df1 = self.__subdf.iloc[:, range(kwhJanIdx,
+                                         kwhJanIdx+12)].max().plot.bar(ax=ax1)
         ax1.set_title('KWH max')
-        
-        
+
         figure1 = plt.figure(dpi=50)
         ax1 = figure1.add_subplot(111)
         bar1 = FigureCanvasTkAgg(figure1, root)
         bar1.get_tk_widget().place(x=310, y=295, width=250, height=155)
         kwhJanIdx = (self.__subdf.columns.get_loc('THERM JANUARY 2010'))
         print(kwhJanIdx)
-        df1 = self.__subdf.iloc[:, range(kwhJanIdx, kwhJanIdx+12)].max().plot.bar(ax=ax1)
+        df1 = self.__subdf.iloc[:, range(kwhJanIdx,
+                                         kwhJanIdx+12)].max().plot.bar(ax=ax1)
         ax1.set_title('THERM max')
-       
+
+# %% Run
+
 
 if __name__ == "__main__":
     root = tk.Tk()
